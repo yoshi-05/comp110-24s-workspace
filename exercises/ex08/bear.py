17b3d5957d2ba0c367dbcc300cd4c6f3ac6e1942
abyrnes1 <abyrnes1@live.unc.edu>
2024-01-07 22:26:49 -0500

    First Commit

--- exercises/ex08/bear.py
+++
@@ -1,9 +0,0 @@
-"""File to define Bear class"""
-
-class Bear:
-    
-    def __init__(self):
-        return None
-    
-    def one_day(self):
-        return None
--- exercises/ex08/fish.py
+++
@@ -1,9 +0,0 @@
-"""File to define Fish class"""
-
-class Fish:
-    
-    def __init__(self):
-        return None
-    
-    def one_day(self):
-        return None
