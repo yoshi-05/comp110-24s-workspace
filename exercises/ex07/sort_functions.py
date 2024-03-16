a22d0e65f50d1e9d8154b1ec0f427175cbc09359
abyrnes1 <abyrnes1@live.unc.edu>
2024-03-16 12:52:44 -0400

    Added ex07 files

---
+++ exercises/ex07/sort_functions.py
@@ -0,0 +1,15 @@
+"""Functions that implement sorting algorithms."""
+
+__author__ = ""
+
+def insertion_sort(x: list[int]) -> None:
+    """Basic insertion sort algorithm.
+    Insert into an already sorted list."""
+    return None
+
+
+def selection_sort(x: list[int]) -> None:
+    """Basic selection sort algorithm. 
+    Repeatedly find the minimum and swap it."""
+    return None
+    
